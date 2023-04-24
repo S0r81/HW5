42c75b3b45c3be7a76f0a35537b75336b727dd65
FranciscoARoman <faroman1@miners.utep.edu>
2023-04-24 11:11:41 -0600

    Renamed Main class for Compile fix

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 //import dimensions;
 import java.awt.*;
-public class main {
+public class Main { // update main to fix compile
     public static void main(String[] args) {
         Dimension dim = new Dimension(400, 600);
         gameFrame game = new gameFrame(dim);
